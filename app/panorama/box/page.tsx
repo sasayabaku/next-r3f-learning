fd369584e6364a6e09b04c9daa639b94c9c457b8
sasayabaku <sasayabaku@yahoo.co.jp>
2023-05-25 23:14:16 +0000

    Dev panorama viewer box geometry

---
+++ app/panorama/box/page.tsx
@@ -0,0 +1,57 @@
+"use client"
+
+import * as THREE from "three"
+import { Canvas, useThree } from "@react-three/fiber"
+import { StrictMode, Suspense, useEffect } from "react"
+import { OrbitControls } from "@react-three/drei"
+
+import { Perf } from "r3f-perf"
+
+const PATH = "/img/panorama/"
+const EXT = ".png"
+// const TEXTURES = ["01_back", "01_right", "01_front", "01_left", "01_bottom", "01_top"]
+const TEXTURES = ["01_right", "01_left", "01_top", "01_bottom", "01_front", "01_back"]
+const IMAGEPATHES = TEXTURES.map((img) => PATH + img + EXT)
+
+export default function Page() {
+  return (
+    <>
+      <div className='w-full h-screen'>
+        <StrictMode>
+          <Canvas
+            gl={{
+              antialias: true,
+              toneMapping: THREE.ACESFilmicToneMapping,
+              outputEncoding: THREE.sRGBEncoding,
+            }}
+          >
+            <OrbitControls makeDefault />
+            <Perf />
+
+            <Suspense fallback={<p>Loading ...</p>}>
+              <Scene />
+            </Suspense>
+          </Canvas>
+        </StrictMode>
+      </div>
+    </>
+  )
+}
+
+function Scene() {
+  useSkybox(IMAGEPATHES)
+  return null
+}
+
+function useSkybox(path: Array<string>) {
+  const { scene } = useThree()
+
+  useEffect(() => {
+    const loader = new THREE.CubeTextureLoader()
+    const mat = loader.load(path)
+    scene.background = mat
+    scene.environment = mat
+  }, [path, scene, scene.background])
+
+  return null
+}
