cdb1b213cf03717b5a000bf91c4c8284a85857d6
sasayabaku <sasayabaku@gmail.com>
2024-02-11 23:50:50 +0900

    model-viewerの導入

--- app/modeling/viewer/page.tsx
+++ app/modeling/viewer/page.tsx
@@ -3,12 +3,22 @@
 import { StrictMode } from 'react';
 
 import ViewerCanvas from './viewerCanvas';
+import '@google/model-viewer';
+
+function ArViewer() {
+  return (
+    <div>
+      <model-viewer src="/dog.glb" camera-controls ar tone-mapping="commerce" />
+    </div>
+  );
+}
 
 export default function Page() {
   return (
     <div className="w-full h-full">
       <StrictMode>
-        <ViewerCanvas />
+        <ArViewer />
+        {/* <ViewerCanvas /> */}
         <div>aaa</div>
       </StrictMode>
     </div>
